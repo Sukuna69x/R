fc3674dcb9ca0224b7151d077ca0983914442379
Sukuna69x <niloygain29@gmail.com>
2025-03-10 12:15:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,11 +15,8 @@ from telegram.ext import (
     ConversationHandler,
 )
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logger = logging.getLogger(name)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)  # Correct way to get logger
 
 # States for conversation handler
 UPLOAD, CHOOSE_QUALITY, RENAME, THUMBNAIL = range(4)
